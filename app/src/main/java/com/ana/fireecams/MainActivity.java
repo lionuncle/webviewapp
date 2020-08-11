3379869b4b77a67406214653b49d23bc1b396ad4
Lionuncle <goninside@gmail.com>
2020-08-11 22:52:35 +0500

    Bug Fixing | privacy policy, terms, onesignal, kocha, Splash, name

--- app/src/main/java/com/ana/fireecams/MainActivity.java
+++ app/src/main/java/com/ana/fireecams/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.RemoteException;
@@ -12,6 +15,7 @@ import android.provider.Settings;
 import android.util.Log;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 import com.android.installreferrer.api.InstallReferrerClient;
 import com.android.installreferrer.api.InstallReferrerClient.InstallReferrerResponse;
@@ -23,7 +27,10 @@ import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.kochava.base.Tracker;
 import com.onesignal.OneSignal;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import static com.android.installreferrer.api.InstallReferrerClient.newBuilder;
 
@@ -40,11 +47,26 @@ public class MainActivity extends AppCompatActivity implements InstallReferrerSt
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getGaid();
-
-
-
-
         webView = findViewById(R.id.webView);
+
+        Uri reveiverIntent = getIntent().getData();
+        try {
+            if (reveiverIntent != null) {
+                String openUrl = new URL(reveiverIntent.toString()).toString();
+                if (openUrl != null){
+                    webView.loadUrl(openUrl);
+                    return;
+                }
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        Tracker.configure(new Tracker.Configuration(getApplicationContext())
+                .setAppGuid("kofireecams-tksg")
+        );
+
+
         mReferrerClient = newBuilder(this).build();
         mReferrerClient.startConnection(this);
 
@@ -132,7 +154,11 @@ public class MainActivity extends AppCompatActivity implements InstallReferrerSt
         url = "http://fircamernw.club?utm_source=fircamernw_aosapp&device_id="+deviceId+"&push-token="+pushToken+"&kd_id="+kdId+"&ref="+ref+"&gaid="+gaid;
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
-        webView.loadUrl(url);
+        Uri reveiverIntent = getIntent().getData();
+
+        if (reveiverIntent == null){
+            webView.loadUrl(url);
+        }
     }
 
     @Override
@@ -141,15 +167,43 @@ public class MainActivity extends AppCompatActivity implements InstallReferrerSt
     }
     @Override
     public void onBackPressed() {
+        if (webView.getUrl().equals("http://fircamernw.club/privacy-policy.html") || webView.getUrl().equals("http://fircamernw.club/terms.html")){
+            MainActivity.super.onBackPressed();
+            return;
+        }
         new AlertDialog.Builder(this)
                 .setMessage("Are you sure you want to exit?")
                 .setCancelable(false)
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
+                        deleteCache(MainActivity.this);
                         MainActivity.super.onBackPressed();
                     }
                 })
                 .setNegativeButton("No", null)
                 .show();
+    }
+    public static void deleteCache(Context context) {
+        try {
+            File dir = context.getCacheDir();
+            deleteDir(dir);
+        } catch (Exception e) { e.printStackTrace();}
+    }
+
+    public static boolean deleteDir(File dir) {
+        if (dir != null && dir.isDirectory()) {
+            String[] children = dir.list();
+            for (int i = 0; i < children.length; i++) {
+                boolean success = deleteDir(new File(dir, children[i]));
+                if (!success) {
+                    return false;
+                }
+            }
+            return dir.delete();
+        } else if(dir!= null && dir.isFile()) {
+            return dir.delete();
+        } else {
+            return false;
+        }
     }
 }
